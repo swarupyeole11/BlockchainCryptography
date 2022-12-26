9b8f4c6817d3bf9af82b140747949fc5e76a478c
Swarup Yeole <76248886+swarupyeole11@users.noreply.github.com>
2022-12-26 11:12:18 +0000

    Defined the Sign Function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 from cryptography.hazmat.primitives.asymmetric import rsa
-
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 #generating private key using rsa 
+#make function to sign the message 
+#make a verification function => uses pu , sign , message
 def gneratekeys():
    private_key = rsa.generate_private_key(
      public_exponent=65537,
@@ -11,8 +14,21 @@ def gneratekeys():
    public_key = private_key.public_key()
    return private_key,public_key
 
+def sign(message,private_key):
+   message=bytes(message,'utf-8')
+   signature = private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+  )
+   return signature
 
 
 if __name__=="__main__":
    pr,pu=gneratekeys()
    print(pr,pu)
+
+   messag
